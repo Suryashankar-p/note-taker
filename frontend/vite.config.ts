50b9bb3df4363249d8d5f1d254daf367eb0f8292
NidhinThoughtminds <nidhin.babu@thoughtminds.io>
2025-05-02 19:14:32 +0530

    base url changed to /api2/

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/api2/',
   preview: {
     port: 3000
   },
