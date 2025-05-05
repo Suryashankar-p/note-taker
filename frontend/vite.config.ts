ba9704b8adad4d772a6bd003cf29d64101333655
Ashwin <tmpl.ad@thermaxglobal.com>
2025-05-05 13:24:43 +0000

    Modified index file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,31 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/api2/',
+//   preview: {
+//     port: 3000
+//   },
+//   server: {
+//     port: 3000,
+//   },
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/api2/',
+  build: {
+    outDir: 'dist/api2',     // put all built files under /api2/
+    assetsDir: 'assets',     // keeps assets in /api2/assets
+  },
   preview: {
     port: 3000
   },
   server: {
-    port: 3000,
-  },
+    port: 3000
+  }
 })
