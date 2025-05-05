3768baa8a4341f076e34cbe42a32414510b31ecd
Ashwin <tmpl.ad@thermaxglobal.com>
2025-05-05 12:33:07 +0000

    Modified docker file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,7 +10,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/api1/"> 
+  <BrowserRouter basename="/api2/"> 
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <App />
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/api1/',
+  base: '/api2/',
   preview: {
     port: 3000
   },
