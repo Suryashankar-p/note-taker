99eb6f909d72221672df6df854bf8b297c14f767
Ashwin <tmpl.ad@thermaxglobal.com>
2025-05-06 11:18:03 +0000

    added global file

---
+++ frontend/src/global.d.ts
@@ -0,0 +1,19 @@
+// src/types/env.d.ts
+export {};
+
+declare global {
+  interface Window {
+    env: {
+      ENABLE_SSO: string;
+      ADMIN_USER_EMAIL: string;
+      DOMAIN: string;
+      URL_PREFIX: string;
+      BACKEND_SSO_URL: string;
+      BACKEND_THERMAX_GPT_URL: string;
+      BACKEND_DOCTOR_CONBOT_URL: string;
+      BACKEND_SMART_TROUBLESHOOT_URL: string;
+      BACKEND_SALES_URL: string;
+      BACKEND_TBWES_OCR_URL: string;
+    };
+  }
+}
