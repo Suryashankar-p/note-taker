304ebe68418824f32803d6f97211f61021a12db0
Ashwin <tmpl.ad@thermaxglobal.com>
2025-05-06 15:37:08 +0000

    Changed path to genaistudio

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,7 +10,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/api2/"> 
+  <BrowserRouter basename="/genaistudio/"> 
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <App />
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,14 +15,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-console.log("VITE_BACKEND_SSO_URL at build:", process.env.VITE_BACKEND_SSO_URL);
-console.log("VITE_BACKEND:", process.env);
 export default defineConfig({
   plugins: [react()],
-  base: '/api2/',
+  base: '/genaistudio/',
   build: {
-    outDir: 'dist/api2',     // put all built files under /api2/
-    assetsDir: 'assets',     // keeps assets in /api2/assets
+    outDir: 'dist/genaistudio',     // put all built files under /genaistudio/
+    assetsDir: 'assets',     // keeps assets in /genaistudio/assets
   },
   preview: {
     port: 3000
