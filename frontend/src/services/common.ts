37019b96d6d841aa3bd50a711be9ecddfe40bc21
nidhinthoughtminds <nidhin.babu@thoughtminds.io>
2025-05-14 12:28:57 +0000

    fix: chnaged ROOT PATH and made changes to take it from env

--- frontend/src/services/common.ts
+++ frontend/src/services/common.ts
@@ -1,6 +1,6 @@
 import {SSOAPI } from "./Axios";
 
-// const BASE_URL = 'http://localhost/api'
+// const ROOT_PATH = 'http://localhost/api'
 // const BACKEND_SSO_URL = import.meta.env.VITE_BACKEND_SSO_URL;
 const BACKEND_SSO_URL = window.env?.BACKEND_SSO_URL || import.meta.env.VITE_BACKEND_SSO_URL;
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,20 +12,26 @@
 //     port: 3000,
 //   },
 // })
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/api2/',
-  build: {
-    outDir: 'dist/api2',     // put all built files under /genaistudio/
-    assetsDir: 'assets',     // keeps assets in /genaistudio/assets
-  },
-  preview: {
-    port: 3000
-  },
-  server: {
-    port: 3000
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const basePath = env.VITE_ROOT_PATH || 'genaistudio'
+
+  return {
+    plugins: [react()],
+    base: `/${basePath}/`,
+    build: {
+      outDir: `dist/${basePath}`, // dynamic output folder
+      assetsDir: 'assets',
+    },
+    preview: {
+      port: 3000,
+    },
+    server: {
+      port: 3000,
+    },
   }
 })
