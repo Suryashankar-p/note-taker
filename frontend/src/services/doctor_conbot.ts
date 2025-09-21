aa9ace8c465bfd974ea2329c62c872cb2d71d4c9
athul-jestin-tm <athul.jestin@thoughtminds.io>
2025-09-21 17:39:25 -0400

    feat: enhance Dr. Conbot service with new chat and category functionalities

--- frontend/src/services/doctor_conbot.ts
+++ frontend/src/services/doctor_conbot.ts
@@ -1,4 +1,3 @@
-// import { deleteAPI, getAPI, patchAPI, postAPI } from "./Axios.ts";
 import store, { Dispatch } from "../redux/store";
 import { fileTypeSelectorDoctorConBot } from "../utils/functions";
 import { DoctorBotAPI } from "./Axios";
@@ -6,10 +5,14 @@ import axios from "axios";
 
 const BACKEND_DOCTOR_CONBOT_URL = import.meta.env.VITE_BACKEND_DOCTOR_CONBOT_URL || window.env?.BACKEND_DOCTOR_CONBOT_URL;
 
+//<====================================Dr. Conbot Auth========================================>
+
 export const GetMemberDoctorConbotRole = async () => {
   const response = await DoctorBotAPI.get(BACKEND_DOCTOR_CONBOT_URL + "/doctor_conbot/member/me");
   return response;
 };
+
+//<====================================Dr. Conbot chat========================================>
 
 export const GetAllChatLists = async (
   skip: number = 0,
@@ -31,7 +34,11 @@ export const DeleteAllChatList = async () => {
 };
 
 export const CreateChat = async (title: string) => {
-  const response = await DoctorBotAPI.post(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/chat?title=${title}`);
+  let body = {
+    title: title,
+  };
+  console.log(body);
+  const response = await DoctorBotAPI.post(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/chat/`,body);
   return response;
 };
 
@@ -46,6 +53,8 @@ export const DeleteChat = async (chat_id: number) => {
   const response = await DoctorBotAPI.delete(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/chat/${chat_id}`);
   return response;
 };
+
+//<====================================Dr. Conbot chat history========================================>
 
 export const ReadChatHistories = async (
   skip: number = 0,
@@ -62,11 +71,21 @@ export const ReadChatHistories = async (
 };
 
 export const CreateChatHistory = async (query: string, chat_id: string) => {
-  const body={
+  let body={
     human: query
   }
   const response = await DoctorBotAPI.post(
     BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/chat/${chat_id}/chat_history`, body
+  );
+  return response;
+};
+
+export const CreateAgentChatHistory = async (query: string, chat_id: string) => {
+  let body={
+    human: query
+  }
+  const response = await DoctorBotAPI.post(
+    BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/chat/${chat_id}/agent/`, body
   );
   return response;
 };
@@ -82,7 +101,7 @@ export const DeleteChatHistory = async (
 };
 
 
-//<Members>
+//<====================================Dr. Conbot member========================================>
 
 export const ReadMembers = async (skip: number = 0, limit: number = 100, search_term?: string) => {
   const response = await DoctorBotAPI.get(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/member?skip=${skip}&limit=${limit}${search_term !== '' ? '&search_term=' + search_term : ''}`)
@@ -104,7 +123,7 @@ export const DeleteMember = async (member_id: string) => {
   return response
 }
 
-//<<<<<<Usage APIs>>>>>>
+//<====================================Dr. Conbot usage========================================>
 
 export const ReadCostUsage = async (year: string | number, month: string | number) => {
   const response = await DoctorBotAPI.get(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/usage/cost?year=${year}&month=${month}`)
@@ -126,13 +145,12 @@ export const ReadActivityUsage = async (year: string | number, month: string | n
   return response
 }
 
-export const ReadActivityUsageTopUsers = async (year: string | number, month: string | number, n: number) => {
-  const response = await DoctorBotAPI.get(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/usage/activity/top?year=${year}&month=${month}&n=${n}`)
+export const ReadActivityUsageTopUsers = async (year: string | number, month: string | number, skip: number = 0, limit: number = 6) => {
+  const response = await DoctorBotAPI.get(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/usage/activity/top?skip=${skip}&limit=${limit}&year=${year}&month=${month}`)
   return response
 }
 
-
-//<<Products>>
+//<====================================Dr. Conbot product========================================>
 
 export const ReadProducts = async (skip: number = 0, limit: number = 100, search_term?: string) => {
   const response = await DoctorBotAPI.get(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/product?skip=${skip}&limit=${limit}${search_term !== '' ? '&search_term=' + search_term : ''}`)
@@ -140,20 +158,20 @@ export const ReadProducts = async (skip: number = 0, limit: number = 100, search
 }
 
 export const CreateProduct = async (title: string, short_title: string, description: string) => {
-  const body={
+  let body={
     title: title,
-    short_title:short_title,
-    description:description 
+    description:description, 
+    ...(short_title ? { short_title } : {})
   }
   const response = await DoctorBotAPI.post(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/product`,body);
   return response
 }
 
 export const UpdateProduct = async (product_id: string, title: string, short_title: string, description: string) => {
-  const body={
+  let body={
     title: title,
-    short_title:short_title,
-    description:description 
+    description:description,
+    ...(short_title ? { short_title } : {})
   }
   const response = await DoctorBotAPI.patch(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/product/${product_id}?title=${title}&short_title=${short_title}&description=${description}`, body)
   return response
@@ -164,14 +182,10 @@ export const DeleteProduct = async (product_id: string) => {
   return response
 }
 
+//<====================================Dr. Conbot product document========================================>
+
 export const ReadProductDocuments = async (product_id: number | string, skip: number = 0, limit: number = 0, search_term?: string) => {
   const response = await DoctorBotAPI.get(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/product/${product_id}/document?skip=${skip}&limit=${limit}${search_term !== '' ? '&search_term=' + search_term : ''}`)
-  return response
-}
-
-
-export const ReadFaqDocuments = async ( skip: number = 0, limit: number = 0, search_term?: string) => {
-  const response = await DoctorBotAPI.get(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/faq?skip=${skip}&limit=${limit}${search_term !== '' ? '&search_term=' + search_term : ''}`)
   return response
 }
 
@@ -188,7 +202,7 @@ export const CreateProductDocument = async (
   if (file) {
     formData.append('document', file, file.name);
   }
-  // formData.append('models', JSON.stringify(models));
+  
   try {
     const response = await axios.post(
       `${BACKEND_DOCTOR_CONBOT_URL}/doctor_conbot/product/${productId}/document${product_document_id ? '/' + product_document_id : ''}?description=${description}&kind=${fileTypeSelectorDoctorConBot(kind)}`,
@@ -212,14 +226,14 @@ export const EditProductDocument = async (
   productId: number | string,
   description: string,
   kind: string,
-  // file: File,
-  // models: any,
+ 
   product_document_id?: string | number
 ) => {
   const token = localStorage.getItem('access_token');
   const formData = new FormData();
   formData.append("description", description);
   formData.append("kind", fileTypeSelectorDoctorConBot(kind));
+ 
   try {
     const response = await axios.patch(
       `${BACKEND_DOCTOR_CONBOT_URL}/doctor_conbot/product/${productId}/document${product_document_id ? '/' + product_document_id : ''}`,
@@ -239,12 +253,112 @@ export const EditProductDocument = async (
   }
 };
 
+export const DeleteProductDocument = async (product_id: string | number, product_document_id: string | number) => {
+  const response = await DoctorBotAPI.delete(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/product/${product_id}/document/${product_document_id}`)
+  return response
+}
+
+export const ReadProductDocumentUrl = async (product_id: string | number, product_document_id: string | number) => {
+  const response = await DoctorBotAPI.get(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/product/${product_id}/document/${product_document_id}/link`)
+  return response
+}
+
+export const ReadFaqDocumentUrl = async (product_document_id: string | number) => {
+  const response = await DoctorBotAPI.get(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/faq/${product_document_id}/link`)
+  return response
+}
+
+export const PollDocumentStatus = async (product_document_id: string | number) => {
+  const response = await DoctorBotAPI.get(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/product/{product_id}/document/${product_document_id}/status`);
+  return response;
+};
+
+//<====================================Dr. Conbot category========================================>
+
+export const ReadCategories = async (skip: number = 0, limit: number = 100, search_term?: string) => {
+  const response = await DoctorBotAPI.get(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/category?skip=${skip}&limit=${limit}${search_term !== '' ? '&search_term=' + search_term : ''}`)
+  return response
+}
+
+export const CreateCategory = async (body: any) => {
+  const response = await DoctorBotAPI.post(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/category/`,body);
+  return response
+}
+
+export const DeleteCategory = async (product_id: string) => {
+  const response = await DoctorBotAPI.delete(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/category/${product_id}`)
+  return response
+}
+
+//<====================================Dr. Conbot category document========================================>
+
+export const ReadCategoryDocuments = async (category_id: number | string, skip: number = 0, limit: number = 0, search_term?: string) => {
+  const response = await DoctorBotAPI.get(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/category/${category_id}/document?skip=${skip}&limit=${limit}${search_term !== '' ? '&search_term=' + search_term : ''}`)
+  return response
+}
+
+export const CreateCategoryDocument = async (
+  categoryId: number | string,
+  description: string,
+  kind: string,
+  file: File,
+) => {
+  const token = localStorage.getItem('access_token');
+  const formData = new FormData();
+
+  if (file) {
+    formData.append('document', file, file.name);
+  }
+
+  // 👇 add these as form fields instead of query params
+  formData.append('description', description);
+  formData.append('kind', fileTypeSelectorDoctorConBot(kind));
+
+  try {
+    const response = await axios.post(
+      `${BACKEND_DOCTOR_CONBOT_URL}/doctor_conbot/category/${categoryId}/document/`,
+      formData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          'Authorization': `Bearer ${token}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error: any) {
+    if (error?.response?.status === 415) {
+      (store.dispatch as Dispatch).toast.openToast({
+        status: true,
+        message:
+          "Unsupported extension(s). Only use .pdf, .xls, .xlsx, .jpg, .jpeg, .mp4",
+      });
+    }
+    throw error;
+  }
+};
+
+export const DeleteCategoryDocument = async (category_id: string | number, category_document_id: string | number) => {
+  const response = await DoctorBotAPI.delete(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/category/${category_id}/document/${category_document_id}`)
+  return response
+}
+
+export const ReadCategoryDocumentUrl = async (category_id: string | number, category_document_id: string | number) => {
+  const response = await DoctorBotAPI.get(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/category/${category_id}/document/${category_document_id}/link`)
+  return response
+}
+
+//<====================================Dr. Conbot FAQ========================================>
+
+export const ReadFaqDocuments = async ( skip: number = 0, limit: number = 0, search_term?: string) => {
+  const response = await DoctorBotAPI.get(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/faq?skip=${skip}&limit=${limit}${search_term !== '' ? '&search_term=' + search_term : ''}`)
+  return response
+}
+
 export const CreateFaqDocument = async (
   description: string,
-  // kind: string,
   file: File,
-  // models: any,
-  product_document_id?: string | number
+  category_document_id?: string | number
 ) => {
   const token = localStorage.getItem('access_token');
   const formData = new FormData();
@@ -253,10 +367,9 @@ export const CreateFaqDocument = async (
   }
   formData.append("description", description);
   formData.append("kind", "FAQ"); 
-  // formData.append('models', JSON.stringify(models));
   try {
     const response = await axios.post(
-      `${BACKEND_DOCTOR_CONBOT_URL}/doctor_conbot/faq${product_document_id ? '/' + product_document_id : ''}?description=${description}&kind=FAQ`,
+      `${BACKEND_DOCTOR_CONBOT_URL}/doctor_conbot/faq${category_document_id ? '/' + category_document_id : ''}?description=${description}&kind=FAQ`,
 
       formData,
       {
@@ -274,22 +387,55 @@ export const CreateFaqDocument = async (
   }
 };
 
-export const EditFaqDocument = async (
+export const DeleteFaqDocument = async (category_document_id: string | number) => {
+  const response = await DoctorBotAPI.delete(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/faq/${category_document_id}`)
+  return response
+}
+
+//<====================================Dr. Conbot sub package========================================>
+
+export const ReadSubpackages = async (category_id:number | string, skip: number = 0, limit: number = 100, search_term?: string) => {
+  const response = await DoctorBotAPI.get(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/category/${category_id}/sub_package?skip=${skip}&limit=${limit}${search_term !== '' ? '&search_term=' + search_term : ''}`)
+  return response
+}
+
+export const CreateSubpackage = async (body: any, category_id: number | string) => {
+  const response = await DoctorBotAPI.post(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/category/${category_id}/sub_package/`, body);
+  return response
+}
+
+export const DeleteSubPackage = async (category_id: number | string, sub_package_id: string | number) => {
+  const response = await DoctorBotAPI.delete(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/category/${category_id}/sub_package/${sub_package_id}`)
+  return response
+}
+
+//<====================================Dr. Conbot sub package document========================================>
+
+export const ReadSubPackageDocuments = async (sub_package_id: number | string, skip: number = 0, limit: number = 0, search_term?: string) => {
+  const response = await DoctorBotAPI.get(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/category/sub_package/${sub_package_id}/document?skip=${skip}&limit=${limit}${search_term !== '' ? '&search_term=' + search_term : ''}`)
+  return response
+}
+
+export const CreateSubPackageDocument = async (
+  sub_package_id: number | string,
   description: string,
-  // kind: string,
-  // file: File,
-  // models: any,
-  product_document_id?: string | number
+  kind: string,
+  file: File,
 ) => {
-  const token = localStorage.getItem('access_token');
+    const token = localStorage.getItem('access_token');
   const formData = new FormData();
-  formData.append("description", description);
-  formData.append("kind", "FAQ"); 
-  
+
+  if (file) {
+    formData.append('document', file, file.name);
+  }
+
+  formData.append('description', description);
+  formData.append('kind', fileTypeSelectorDoctorConBot(kind));
+
   try {
-    const response = await axios.patch(
-      `${BACKEND_DOCTOR_CONBOT_URL}/doctor_conbot/faq${product_document_id ? '/' + product_document_id : ''}`,
-      formData, 
+    const response = await axios.post(
+      `${BACKEND_DOCTOR_CONBOT_URL}/doctor_conbot/category/sub_package/${sub_package_id}/document/`,
+      formData,
       {
         headers: {
           'Content-Type': 'multipart/form-data',
@@ -297,31 +443,21 @@ export const EditFaqDocument = async (
         },
       }
     );
-    
     return response.data;
   } catch (error: any) {
-    if (error?.response?.status === 415)
-      (store.dispatch as Dispatch).toast.openToast({ status: true, message: "Unsupported extension(s) only use .pdf, .xls, .xlsx ,.jpg, .jpeg ,.mp4" })
+    if (error?.response?.status === 415) {
+      (store.dispatch as Dispatch).toast.openToast({
+        status: true,
+        message:
+          "Unsupported extension(s). Only use .pdf, .xls, .xlsx, .jpg, .jpeg, .mp4",
+      });
+    }
     throw error;
   }
 };
 
-
-export const DeleteProductDocument = async (product_id: string | number, product_document_id: string | number) => {
-  const response = await DoctorBotAPI.delete(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/product/${product_id}/document/${product_document_id}`)
-  return response
-}
-export const DeleteFaqDocument = async ( product_document_id: string | number) => {
-  const response = await DoctorBotAPI.delete(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/faq/${product_document_id}`)
+export const DeleteSubPackageDocument = async (sub_package_document_id: string | number,sub_package_id: string | number) => {
+  const response = await DoctorBotAPI.delete(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/category/sub_package/${sub_package_id}/document/${sub_package_document_id}`)
   return response
 }
 
-export const ReadProductDocumentUrl = async (product_id: string | number, product_document_id: string | number) => {
-  const response = await DoctorBotAPI.get(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/product/${product_id}/document/${product_document_id}/link`)
-  return response
-}
-
-export const ReadFaqDocumentUrl = async (product_document_id: string | number) => {
-  const response = await DoctorBotAPI.get(BACKEND_DOCTOR_CONBOT_URL + `/doctor_conbot/faq/${product_document_id}/link`)
-  return response
-}
