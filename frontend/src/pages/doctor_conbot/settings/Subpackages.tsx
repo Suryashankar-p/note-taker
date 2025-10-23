0e57165bc15c7dfa232a74e938731dbd70f9740a
athul-jestin-tm <athul.jestin@thoughtminds.io>
2025-10-23 05:30:47 -0400

    fix: Fix on Blob storage issue

--- frontend/src/pages/doctor_conbot/settings/Subpackages.tsx
+++ frontend/src/pages/doctor_conbot/settings/Subpackages.tsx
@@ -323,6 +323,7 @@ const Subpackages: React.FC<SubpackagesProps> = ({ onSwitch, productData }) => {
             type: getFileType(file?.filename),
             url: linkResp?.link,
           };
+          console.log("Base link:",fileInfo.url)
           setFileData(fileInfo);
           setFileShow(true);
         } else {
