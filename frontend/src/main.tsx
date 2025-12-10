641a72d6cfd1478e5f0cb09c55d92a006976b75c
athul-jestin-tm <athul.jestin@thoughtminds.io>
2025-12-10 07:28:14 -0500

    fix: Remove 'genaistudio1' references from configuration and scripts for cleaner routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,11 +7,11 @@ import store from "./redux/store.ts";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // Need to change this line
-const ROOT_PATH = 'genaistudio1'
+// const ROOT_PATH = 'genaistudio1'
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename={`/${ROOT_PATH}/`}> 
+  <BrowserRouter basename={`/`}> 
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <App />
