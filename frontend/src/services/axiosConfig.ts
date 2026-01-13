da88ec0df33c298f36487226edde28eb3426ec14
athul-jestin-tm <athul.jestin@thoughtminds.io>
2026-01-13 07:44:58 -0500

    feat: Improve error handling in axios interceptors and update Dashboard navigation logic

--- frontend/src/services/axiosConfig.ts
+++ frontend/src/services/axiosConfig.ts
@@ -25,32 +25,31 @@ export const setInterceptors = (axiosInstance: AxiosInstance) => {
       const { response }: any = error;
 
       if (response) {
-        
         switch (response.status) {
           case 400:
-            break
+            console.error('Bad Request:', response.data);
+            return response;
           case 401:
-            redirectToLogin()
-            break
+            console.error('Unauthorized:', response.data);
+            return response;
           case 403:
-            redirectToLogin()
-            break
+            console.error('Forbidden:', response.data);
+            return response;
           case 404:
-            break
+            console.error('Not Found:', response.data);
+            return response;
           case 412:
-            break
-          case 502:
-            console.error(response.statusText, response.data);
-            break;
+            console.error('Not Found:', response.data);
+            return response
           case 500:
-            dispatch.toast.openToast({
-              status: true,
-              message: 'Server did not respond',
-              type: 'error',
-            });
-            break;
+            console.error('Internal Server Error:', response.data);
+            dispatch.toast.openToast({ status: true, message: "Server did not respond", type: 'error' });
+          case 502:
+            console.error('Bad Gateway:', response.data);
+            return response;
           default:
-            console.error('Unhandled error:', response.data);
+            console.error(`Error: ${response.status}`, response.data);
+            return response;
         }
       } else if (error.message === 'Network Error') {
         dispatch.toast.openToast({
