db1fd80719dcbd19d642d98cfda4fd892adb2c5b
tm-ajestin <tm.ajestin@thermaxglobal.com>
2026-02-19 19:03:58 +0530

    fix: Update API endpoints to use BACKEND_HEATING_OCR_URL for consistency

--- frontend/src/services/heating_ocr.ts
+++ frontend/src/services/heating_ocr.ts
@@ -7,57 +7,57 @@ const BACKEND_HEATING_OCR_URL=import.meta.env.VITE_BACKEND_HEATING_OCR_URL  || w
 /////////////////<<<<<<<<<<<<<<<<<<<OCR APIs>>>>>>>>>>>>>>>>>>>>>\\\\\\\\\\\\\\\\\\
 
 export const GetHeatingOCRRole = async () => {
-    const response = await HeatingOCRAPI.get('/heating_ocr/member/me')
+    const response = await HeatingOCRAPI.get(BACKEND_HEATING_OCR_URL + '/heating_ocr/member/me')
     return response
   }
   
   export const GetOCRActivities = async (skip: number = 0, limit: number = 100, search_term?: string, user_status?: string, status?: string) => {
-    const response = await HeatingOCRAPI.get(`/heating_ocr/activity?skip=${skip}&limit=${limit}${search_term !== '' ? '&search_term=' + search_term : ''}${status ? '&status=' + status : ''}${user_status ? '&user_status=' + user_status : ''}`)
+    const response = await HeatingOCRAPI.get(BACKEND_HEATING_OCR_URL + `/heating_ocr/activity?skip=${skip}&limit=${limit}${search_term !== '' ? '&search_term=' + search_term : ''}${status ? '&status=' + status : ''}${user_status ? '&user_status=' + user_status : ''}`)
     return response
   }
   
   export const GetOCRActivitiesDetails = async(activity_id: number) => {
-    const response = await HeatingOCRAPI.get(`/heating_ocr/activity/${activity_id}`)
+    const response = await HeatingOCRAPI.get(BACKEND_HEATING_OCR_URL + `/heating_ocr/activity/${activity_id}`)
     return response
   }
   
   export const UpdateOCRActivitiesDetails = async(activity_id: number, body?: any) => {
-    const response = await HeatingOCRAPI.patch(`/heating_ocr/activity/${activity_id}`, body);
+    const response = await HeatingOCRAPI.patch(BACKEND_HEATING_OCR_URL + `/heating_ocr/activity/${activity_id}`, body);
     return response
   }
 
   export const GetMakerCodes = async (search = "") => {
-    const response = await HeatingOCRAPI.get(`/heating_ocr/maker_code/${search ? `?search=${search}` : ""}`);
+    const response = await HeatingOCRAPI.get(BACKEND_HEATING_OCR_URL + `/heating_ocr/maker_code/${search ? `?search=${search}` : ""}`);
     return response;
   };
 
   export const GetProcessCodes = async (search = "") => {
-    const response = await HeatingOCRAPI.get(`/heating_ocr/process_code/${search ? `?search=${search}` : ""}`);
+    const response = await HeatingOCRAPI.get(BACKEND_HEATING_OCR_URL + `/heating_ocr/process_code/${search ? `?search=${search}` : ""}`);
     return response;
   };
   
   export const DeleteOCRActivities = async(activity_id: number) => {
-    const response = await HeatingOCRAPI.delete(`/heating_ocr/activity/${activity_id}`)
+    const response = await HeatingOCRAPI.delete(BACKEND_HEATING_OCR_URL + `/heating_ocr/activity/${activity_id}`)
     return response
   }
   
   export const ReadOCRMembers = async (skip: number = 0, limit: number = 100, search_term?: string) => {
-    const response = await HeatingOCRAPI.get(`/heating_ocr/member?skip=${skip}&limit=${limit}${search_term !== '' ? '&search_term=' + search_term : ''}`)
+    const response = await HeatingOCRAPI.get(BACKEND_HEATING_OCR_URL + `/heating_ocr/member?skip=${skip}&limit=${limit}${search_term !== '' ? '&search_term=' + search_term : ''}`)
     return response
   }
   
   export const CreateOCRMember = async (role: string, email: string, name: string) => {
-    const response = await HeatingOCRAPI.post(`/heating_ocr/member?role=${role}&email=${email}&name=${name}`)
+    const response = await HeatingOCRAPI.post(BACKEND_HEATING_OCR_URL + `/heating_ocr/member?role=${role}&email=${email}&name=${name}`)
     return response
   }
   
   export const UpdateOCRMember = async (role: string, name: string, member_id: string) => {
-    const response = await HeatingOCRAPI.patch(`/heating_ocr/member/${member_id}?name=${name}&role=${role}`)
+    const response = await HeatingOCRAPI.patch(BACKEND_HEATING_OCR_URL + `/heating_ocr/member/${member_id}?name=${name}&role=${role}`)
     return response
   }
   
   export const DeleteOCRMember = async (member_id: string) => {
-    const response = await HeatingOCRAPI.delete(`/heating_ocr/member/${member_id}`)
+    const response = await HeatingOCRAPI.delete(BACKEND_HEATING_OCR_URL + `/heating_ocr/member/${member_id}`)
     return response
   }
   
